c6e856fa77a3ddd358886592c619076d160d3012
Ilya Protsenko <ilya.protsenko@a2ccloud.com>
2023-01-30 15:28:34 +0200

    Task manager with react-beautiful-dnd

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from "uuid";
 const data = [{
     id: 1,
     icon: "⭕️",
@@ -45,6 +46,78 @@ const statuses = [{
     icon: "✅",
     color: "#3981DE"
 }];
+
+
+
+export const dataForReactBeautifulDnD = [
+  {
+    id: "1",
+    Task: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent.",
+    order: 1,
+    // Assigned_To: 'Beltran',
+    // Assignee: 'Romona',
+    // Status: 'To-do',
+    // Priority: 'Low',
+    Due_Date: "25-May-2020"
+  },
+  {
+    id: "3",
+    Task: "Handle Door Specs",
+    order: 2,
+    // Assigned_To: 'Roman',
+    // Assignee: 'Romona',
+    // Status: 'To-do',
+    // Priority: 'Low',
+    Due_Date: "27-May-2020"
+  },
+  {
+    id: "4",
+    Task: "morbi",
+    order: 3,
+
+    // Assigned_To: 'Gawen',
+    // Assignee: 'Kai',
+    // Status: 'Done',
+    // Priority: 'High',
+    Due_Date: "23-Aug-2020"
+  },
+  {
+    id: "5",
+    Task: "proin",
+    order: 4,
+    // Assigned_To: 'Bondon',
+    // Assignee: 'Antoinette',
+    // Status: 'In Progress',
+    // Priority: 'Medium',
+    Due_Date: "05-Jan-2021"
+  },
+  {
+    id: "2",
+    Task: "Fix Styling",
+    order: 5,
+
+    // Assigned_To: 'Dave',
+    // Assignee: 'Romona',
+    // Status: 'To-do',
+    // Priority: 'Low',
+    Due_Date: "26-May-2020"
+  }
+];
+
+export const columnsFromBackend = {
+  [uuidv4()]: {
+    title: "To-do",
+    items: dataForReactBeautifulDnD
+  },
+  [uuidv4()]: {
+    title: "In Progress",
+    items: []
+  },
+  [uuidv4()]: {
+    title: "Done",
+    items: []
+  }
+};
 
 
 export { data, statuses };
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,74 +1,90 @@
 import React, { useState } from "react";
-import { Col } from "../components/Col";
-import { DropWrapper } from "../components/DropWrapper";
-import { Motion, spring } from 'react-motion';
-import { Item } from "../components/Item";
-import {data, statuses} from '../data'
-import FlipMove from 'react-flip-move';
-import {Draggable} from 'react-beautiful-dnd' 
-
-import update from 'immutability-helper'
+import {columnsFromBackend} from '../data'
+import {DragDropContext, Droppable} from 'react-beautiful-dnd' 
+import TaskCard from "../components/TaskCard";
 
 const Homepage = props => {
-    const [items, setItems] = useState(data)
-    console.log('items', items)
-    const findCard = (id) => {
-        const card = items.find(item => item.id === id)
+    const [columns, setColumns] = useState(columnsFromBackend);
 
-        return {
-            card,
-            index: items.indexOf(card)
+    const onDragEnd = (result) => {
+        console.log("result", result);
+
+        if (!result.destination) return;
+
+        const { source, destination } = result;
+
+        if (source.droppableId !== destination.droppableId) {
+       
+          setColumns((prevStateColumns) => { 
+            const sourceColumn = prevStateColumns[source.droppableId];// draggble element -- from
+            const destColumn = prevStateColumns[destination.droppableId];// droppble element -- to
+            const sourceItems = [...sourceColumn.items];
+
+            const destItems = [...destColumn.items];
+            const [removed] = sourceItems.splice(source.index, 1);
+
+            destItems.splice(destination.index, 0, removed);
+
+            return {
+                ...prevStateColumns,
+                [source.droppableId]: {
+                  ...sourceColumn,
+                  items: sourceItems
+                },
+                [destination.droppableId]: {
+                  ...destColumn,
+                  items: destItems
+                }
+              }
+
+          });
+        } else {
+    
+          setColumns((prevStateColumns) => {
+            const column = prevStateColumns[source.droppableId];
+            const copiedItems = [...column.items];
+            const [removed] = copiedItems.splice(source.index, 1);
+            copiedItems.splice(destination.index, 0, removed);
+
+           return {
+                ...prevStateColumns,
+                [source.droppableId]: {
+                  ...column,
+                  items: copiedItems
+                }
+              }
+          });
         }
-    }
-
-    const onDrop = (item, monitor, status) => {
-        const mapping = statuses.find(si => si.status === status)
-        const { card, index } = findCard(item.id);
-
-        const updatedItems =  update(items, {
-            $splice: [
-              [index, 1],
-              [index, 0, {...card, status, icon: mapping.icon}]
-            ],
-          })
-
-        setItems(updatedItems.map((item, idx) => ({...item, order: idx})))
-    }
-
-    const moveItem = (id, atIndex) => {
-
-        const { card, index } = findCard(id);
-        const updatedItems=  update(items, {
-            $splice: [
-              [index, 1],
-              [atIndex, 0, card],
-
-            ],
-            
-          })
-
-        setItems(updatedItems.map((item, idx) => ({...item, order: idx})))
-    }
+      };
 
     return (
-        <div className="row-grid">
-            {statuses.map((s) => (
-                <div key={s.status} className="col-wrapper">
-                    <h2 className="col-header">{s.status.toUpperCase()}</h2>
-                    <DropWrapper onDrop={onDrop} status={s.status}>
-                        <Col >
-                            {items
-                                .filter(i => i.status === s.status)
-                                .sort((a,b) => a.order - b.order)
-                                .map((i, idx) => (
-                                <Item key={i.id} item={i} index={idx} id={i.id} findCard={findCard} moveItem={moveItem} status={s}/>
-                            ))}
-                           
-                        </Col>
-                    </DropWrapper>
-                </div>
-            ))}
+        <DragDropContext
+        onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+      >
+        <div className="container">
+          <div className="task-column-styles">
+            {Object.entries(columns).map(([columnId, column], index) => {
+              return (
+                <Droppable key={columnId} droppableId={columnId}>
+                  {(provided, snapshot) => (
+                    <div
+                    className="task-list"
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                    >
+                      <div className="title">{column.title}</div>
+                      {column.items.map((item, index) => (
+                        <TaskCard key={item.id} item={item} index={index} />
+                      ))}
+                      {provided.placeholder}
+                    </div>
+                  )}
+                </Droppable>
+              );
+            })}
+          </div>
         </div>
+      </DragDropContext>
     );
 };
 
